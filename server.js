e995be23c21b5049c5235451017fc627d173ffb7
unknown <12siricoa94@gmail.com>
2019-10-21 21:18:03 -0400

    added first route into routes.js.

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var routes = require("./routes");
+app.use(routes);
+
 app.listen(PORT, function() {
 
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
-module.exports = connection;
